f12cac1ee7e36ae896fffd19b24e1d63179974e3
Ali Rostami <AliRostami1253@gmail.com>
2025-03-29 13:41:41 +0330

    change access and refresh token config

--- furniture_store/settings.py
+++ furniture_store/settings.py
@@ -167,8 +167,8 @@ from datetime import timedelta
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
 }
 
 SPECTACULAR_SETTINGS = {
